d99fa20d43ba955dcc4629db6e35b55b6a8b116e
fatma bellout <lline1245@gmail.com>
2025-03-05 12:06:46 +0100

    initialisation du projet

---
+++ src/test/java/com/ynov/cvanalyzer/CvanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.cvanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
